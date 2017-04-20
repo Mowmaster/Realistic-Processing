3ff5836f68d18db7ffe4076647d19460f91ae65e
Mowmaster <mowmaster@outlook.com>
2017-04-19 19:19:47 -0500

    Got the Bloomery working minus that you cant pickup the ingots after

--- src/main/java/com/mowmaster/rprocessing/blocks/BlockClayBloomery.java
+++ src/main/java/com/mowmaster/rprocessing/blocks/BlockClayBloomery.java
@@ -10,18 +10,24 @@ import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemFlintAndSteel;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.EnumParticleTypes;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraft.util.text.TextComponentTranslation;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 import javax.annotation.Nullable;
 import java.lang.ref.Reference;
+import java.util.Random;
 
 import static com.mowmaster.rprocessing.configs.RealTab.REAL_TAB;
 
@@ -47,21 +53,109 @@ public class BlockClayBloomery extends Block implements ITileEntityProvider
             TileEntity tileEntity = worldIn.getTileEntity(pos);
             if(tileEntity instanceof TileClayBloomery) {
                 TileClayBloomery bloom = (TileClayBloomery) tileEntity;
-                if(playerIn.isCreative()) {
-                    playerIn.sendMessage(new TextComponentString("You are in Creative"));
-                }
-                else
+
+                if((playerIn.getHeldItem(hand) != null))
                 {
-                    playerIn.sendMessage(new TextComponentString("You are NOT in Creative"));
+                    if(ItemStack.areItemsEqual(playerIn.getHeldItem(hand), new ItemStack(Items.COAL))) {
+                        if(bloom.addCarbon())
+                        {
+                            playerIn.sendMessage(new TextComponentString("You are adding Carbon"));
+                            playerIn.getHeldItem(hand).shrink(1);
+                        }
+                    }
                 }
+                if(playerIn.isSneaking())
+                {
+                    if(bloom.removeCarbon())
+                    {
+                        playerIn.sendMessage(new TextComponentString("You are removing Carbon"));
+                    }
+                }
+
+                if((playerIn.getHeldItem(hand) != null))
+                {
+                    if(ItemStack.areItemsEqual(playerIn.getHeldItem(hand), new ItemStack(Blocks.IRON_ORE))) {
+                        if(bloom.addIron())
+                        {
+                            playerIn.sendMessage(new TextComponentString("You are adding Iron Ore"));
+                            playerIn.getHeldItem(hand).shrink(1);
+                        }
+                    }
+                }
+                if(playerIn.isSneaking())
+                {
+                    if(bloom.removeIron())
+                    {
+                        playerIn.sendMessage(new TextComponentString("You are removing Iron Ore"));
+                    }
+                }
+
+                if((playerIn.getHeldItem(hand) != null))
+                {
+                    if(ItemStack.areItemsEqual(playerIn.getHeldItem(hand), new ItemStack(Blocks.GOLD_ORE))) {
+                        if(bloom.addGold())
+                        {
+                            playerIn.sendMessage(new TextComponentString("You are adding Gold Ore"));
+                            playerIn.getHeldItem(hand).shrink(1);
+                        }
+                    }
+                }
+                if(playerIn.isSneaking())
+                {
+                    if(bloom.removeGold())
+                    {
+                        playerIn.sendMessage(new TextComponentString("You are removing Gold Ore"));
+                    }
+                }
+
+                if((playerIn.getHeldItem(hand) != null))
+                {
+                    if(ItemStack.areItemsEqual(playerIn.getHeldItem(hand), new ItemStack(Items.FLINT_AND_STEEL)))
+                    {
+                        if(bloom.activate())
+                        {
+                            playerIn.sendMessage(new TextComponentString("You have Lit the Bloomery"));
+                        }
+                    }
+
+                    if(ItemStack.areItemsEqual(playerIn.getHeldItem(hand), new ItemStack(Items.WATER_BUCKET)))
+                    {
+                        if(bloom.deactivate())
+                        {
+                            playerIn.sendMessage(new TextComponentString("You have put out the Bloomery"));
+                        }
+                    }
+                }
+
+                if((playerIn.getHeldItem(hand) != null))
+                {
+                    if(ItemStack.areItemsEqual(playerIn.getHeldItem(hand), new ItemStack(Items.REEDS)))
+                    {
+                        if(bloom.addOxygen())
+                        {
+                            bloom.resetNeedsOxygen();
+                            playerIn.sendMessage(new TextComponentString("You have added Oxygen to the Bloomery"));
+                        }
+                    }
+                }
+
+
+
+
+
+
             }
         }
         return true;
     }
+
+
 
     @Nullable
     @Override
     public TileEntity createNewTileEntity(World worldIn, int meta) {
         return new TileClayBloomery();
     }
+
+
 }
--- src/main/java/com/mowmaster/rprocessing/tiles/TileClayBloomery.java
+++ src/main/java/com/mowmaster/rprocessing/tiles/TileClayBloomery.java
@@ -1,44 +1,287 @@
 package com.mowmaster.rprocessing.tiles;
 
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumParticleTypes;
+import net.minecraft.util.ITickable;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+import java.util.Random;
 
 /**
  * Created by KingMowmaster on 4/18/2017.
  */
-public class TileClayBloomery extends TileEntity
+public class TileClayBloomery extends TileEntity implements ITickable
 {
     public static int oxygencount=0;
-    public static int maxoxygen=95; //because if 95 it can go up to 100
+    public static int maxoxygen=250; //because if 95 it can go up to 100
+    public static int needsoxygen = 0;
+    public static int maxneedsoxygen = 100;
     public static int carboncount=0;
     public static int maxcarbon=8;
     public static int maxore=8;
+    public static int orecount=0;
     public static int oreiron=0;
     public static int oregold=0;
+    public static int processtimer = 0;
+    public static int maxprocessedtime = 200;
+
+    public static boolean activated = false;
+    public static boolean processed = false;
 
 
 
-    public void addCarbon()
+    public boolean addCarbon()
     {
         if(carboncount<maxcarbon)
         {
             carboncount++;
+            System.out.println("You are adding Carbon");
+            System.out.println("Carbon Count :" + carboncount);
+            return true;
         }
+        return false;
     }
-    public void removeCarbon()
+    public boolean removeCarbon()
     {
         if(carboncount>0)
         {
+            world.spawnEntity(new EntityItem(this.world, pos.getX() + 0.5,pos.getY() + 1.0,pos.getZ() + 0.5,new ItemStack(Items.COAL)));
             carboncount--;
+            System.out.println("You are removing Carbon");
+            System.out.println("Carbon Count :" + carboncount);
+            return true;
         }
+        return false;
     }
 
+    public boolean activate()
+    {
+        if(carboncount>0)
+        {
+            if(carboncount>=orecount)
+            {
+                if(activated == false)
+                {
+                    activated = true;
+                    needsoxygen = 0;
+                    System.out.println("Activation :" + activated);
+                    return true;
+                }
+            }
 
+        }
+        return false;
+    }
+    public boolean deactivate()
+    {
+        if(activated == true)
+        {
+            activated = false;
+            System.out.println("Activation :" + activated);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean addOxygen()
+    {
+            if(oxygencount<maxoxygen)
+            {
+                oxygencount += 50;
+                System.out.println("You are adding Oxygen");
+                return true;
+            }
+        return false;
+    }
+
+    public void resetNeedsOxygen()
+    {
+        needsoxygen = 0;
+    }
+
+    public boolean addIron()
+    {
+    if(oregold == 0)
+    {
+        if(oreiron<maxore)
+        {
+            oreiron++;
+            orecount++;
+            System.out.println("Iron Count :" + oreiron);
+            return true;
+        }
+    }
+    return false;
+    }
+    public boolean addGold()
+    {
+        if(oreiron == 0)
+        {
+            if(oregold<maxore)
+            {
+                oregold++;
+                orecount++;
+                System.out.println("Gold Count :" + oregold);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean removeIron()
+    {
+        if(oregold == 0)
+        {
+            if(oreiron>0)
+            {
+                world.spawnEntity(new EntityItem(this.world, pos.getX() + 0.5,pos.getY() + 1.0,pos.getZ() + 0.5,new ItemStack(Blocks.IRON_ORE)));
+                oreiron--;
+                orecount--;
+                System.out.println("Iron Count :" + oreiron);
+                return true;
+            }
+        }
+        return false;
+    }
+    public boolean removeGold()
+    {
+        if(oreiron == 0)
+        {
+            if(oregold>0)
+            {
+                world.spawnEntity(new EntityItem(this.world, pos.getX() + 0.5,pos.getY() + 1.0,pos.getZ() + 0.5,new ItemStack(Blocks.GOLD_ORE)));
+                oregold--;
+                orecount--;
+                System.out.println("Gold Count :" + oregold);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public void update()
+    {
+
+        if(activated == true)
+        {
+
+            if(oxygencount>0)
+            {
+                oxygencount--;
+                System.out.println("Oxygen Count :" + oxygencount);
+
+            }
+
+            if(oxygencount == 0)
+            {
+                if(needsoxygen<maxneedsoxygen)
+                {
+                    needsoxygen++;
+                    System.out.println("Oxygen Timer Count :" + needsoxygen);
+                }
+            }
+
+            if(oxygencount > 100)
+            {
+                world.spawnParticle(EnumParticleTypes.SMOKE_LARGE,pos.getX() + 0.5,pos.getY() + 1.0,pos.getZ() + 1, 0.5,0.5,0.5,new int[0]);
+            }
+
+            if(oxygencount < 100)
+            {
+                world.spawnParticle(EnumParticleTypes.SMOKE_NORMAL,pos.getX() + 0.5,pos.getY() + 1.0,pos.getZ() + 1, 0.5,0.5,0.5,new int[0]);
+            }
+
+            if(oxygencount < 50)
+            {
+                world.spawnParticle(EnumParticleTypes.LAVA,pos.getX() + 0.5,pos.getY() + 1.0,pos.getZ() + 1, 0.5,0.5,0.5,new int[0]);
+            }
+
+            if(needsoxygen == maxneedsoxygen)
+            {
+                activated = false;
+                processtimer = 0;
+                System.out.println("Bloomery Off and Reset");
+
+            }
+
+            if(processtimer<maxprocessedtime)
+            {
+                processtimer++;
+            }
+
+            if(processtimer == maxprocessedtime)
+            {
+                processed = true;
+                System.out.println("Bloomery COMPLETED");
+            }
+
+            if(processed == true)
+            {
+                if(oreiron >0)
+                {
+                    world.spawnEntity(new EntityItem(this.world, pos.getX() + 0.5,pos.getY() + 1.0,pos.getZ() + 0.5,new ItemStack(Items.IRON_INGOT,oreiron)));
+                    oreiron = 0;
+                    orecount = 0;
+                }
+                if(oregold >0)
+                {
+                    world.spawnEntity(new EntityItem(this.world, pos.getX() + 0.5,pos.getY() + 1.0,pos.getZ() + 0.5,new ItemStack(Items.GOLD_INGOT,oregold)));
+                    oregold = 0;
+                    orecount = 0;
+                }
+
+                if(oreiron == 0 && oregold == 0)
+                {
+                    activated = false;
+                    oreiron = 0;
+                    oregold = 0;
+                    orecount = 0;
+                    carboncount = 0;
+                    oxygencount = 0;
+                    processtimer = 0;
+                    needsoxygen = 0;
+                    processed = false;
+                    System.out.println("Bloomery Reset");
+                }
+
+            }
+
+
+        }
+
+
+
+
+
+    }
 
     @Override
     public NBTTagCompound writeToNBT(NBTTagCompound compound) {
         super.writeToNBT(compound);
         compound.setInteger("carboncount",carboncount);
+        compound.setBoolean("activated",activated);
+        compound.setInteger("oxygencount",oxygencount);
+        compound.setInteger("needsoxygen", needsoxygen);
+        compound.setInteger("orecount",orecount);
+        compound.setInteger("iron", oreiron);
+        compound.setInteger("gold",oregold);
+        compound.setInteger("timer",processtimer);
+        compound.setBoolean("processed",processed);
+
+
         return compound;
 
     }
@@ -47,6 +290,14 @@ public class TileClayBloomery extends TileEntity
     public void readFromNBT(NBTTagCompound compound) {
         super.readFromNBT(compound);
         compound.getInteger("carboncount");
+        compound.getBoolean("activated");
+        compound.getInteger("oxygencount");
+        compound.getInteger("needsoxygen");
+        compound.getInteger("orecount");
+        compound.getInteger("iron");
+        compound.getInteger("gold");
+        compound.getInteger("timer");
+        compound.getBoolean("processed");
 
 
     }

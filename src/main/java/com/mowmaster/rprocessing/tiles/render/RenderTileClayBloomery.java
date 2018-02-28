4db5d904f5a9c280a91faebf8077e0f088fa3df8
Mowmaster <mowmaster@outlook.com>
2018-02-28 11:54:12 -0600

    some more changes

--- src/main/java/com/mowmaster/rprocessing/tiles/render/RenderTileClayBloomery.java
+++ src/main/java/com/mowmaster/rprocessing/tiles/render/RenderTileClayBloomery.java
@@ -3,16 +3,26 @@ package com.mowmaster.rprocessing.tiles.render;
 import com.mowmaster.rprocessing.tiles.TileClayBloomery;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.GlStateManager;
-import net.minecraft.client.renderer.block.model.ItemCameraTransforms;
+import net.minecraft.client.renderer.RenderItem;
+import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
-import net.minecraft.entity.item.EntityItem;
-import net.minecraft.init.Blocks;
-import net.minecraft.init.Items;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
-
-
+@SideOnly(Side.CLIENT)
 public class RenderTileClayBloomery extends TileEntitySpecialRenderer<TileClayBloomery>
 {
+    public void render(TileClayBloomery te, double x, double y, double z, float partialTicks, int destroyStage, float alpha) {
+        Item item = te.getOreItemInBlock().getItem();
 
+        GlStateManager.pushMatrix();
+        GlStateManager.translate(x, y, z);
+
+
+
+        GlStateManager.popMatrix();
+    }
 }
